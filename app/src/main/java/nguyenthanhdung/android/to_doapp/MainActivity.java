644c58e76e8a3691b07af2cccee29e929bb2eef7
Beater1804 <thanhdung12qh@gmail.com>
2022-05-08 09:44:20 +0700

    Save task on Firebase by dungnt

--- app/src/main/java/nguyenthanhdung/android/to_doapp/MainActivity.java
+++ app/src/main/java/nguyenthanhdung/android/to_doapp/MainActivity.java
@@ -1,14 +1,36 @@
 package nguyenthanhdung.android.to_doapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rvListToDo);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/nguyenthanhdung/android/to_doapp/OnDialogCloseListener.java
@@ -0,0 +1,8 @@
+package nguyenthanhdung.android.to_doapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
